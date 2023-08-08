5883e8c287d555d12cffd2233a4fd23a2fc579f4
TedXpro <johannes.woldeyes@gmail.com>
2023-08-08 12:22:16 +0300

    add starter validation dependency

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,11 +1,13 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -17,7 +19,7 @@ public class TodoController {
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
-    
+
     @RequestMapping("list-todos")
     public String listAllTodos(ModelMap model) {
 
@@ -26,15 +28,18 @@ public class TodoController {
 
         return "listTodos";
     }
-    
+
     @RequestMapping(value = "add-todo", method = RequestMethod.GET)
     public String showNewTodoPage() {
         return "todo";
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodoPage() {
-
+    public String addNewTodoPage(@RequestParam String description, ModelMap model) {
+        String username = (String) model.get("name");
+        todoService.addTodo(username, description,
+                LocalDate.now().plusYears(1), false);
+                
         return "redirect:list-todos"; // redirect: returns to the page that already has some data; it redirects it.
     }
 
